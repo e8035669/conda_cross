12669546c0ca2d1e43c7dc7933f875906c16df41
Yu-Ren Zhang <e8035669@gmail.com>
2023-03-26 06:31:10 +0800

    update Dockerfiles

---
+++ docker/build_images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker buildx build --progress plain -t cfs-base-armv7l cfs-base-armv7l
+docker buildx build --progress plain -t cfs-builder-armv7l cfs-builder-armv7l
+docker buildx build --progress plain -t compiler-builder-armv7l compiler-builder-armv7l
+
