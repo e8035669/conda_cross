5c169fa1bae05fc0d188bb08431dc211b0def108
Yu-Ren Zhang <e8035669@gmail.com>
2023-05-06 22:25:51 +0800

    update

--- recipes3/cross/commit-packages.sh
+++ recipes3/cross/commit-packages.sh
@@ -4,14 +4,16 @@ for i in */; do
     # echo $i
 
     # ret=$(git -C "$i" status | grep "tree clean")
+    # ret=$(git -C "$i" status | grep "乾淨")
 
     # if [ -z "$ret" ]; then
     #     echo $i not clean
     # fi
 
     remote=$(git -C "$i" remote get-url origin)
+    branch=$(git -C "$i" branch --show-current)
 
-    git submodule add "$remote" "$i"
+    git submodule add -f -b "$branch" "$remote" "$i"
 
 done
 
